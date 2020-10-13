85b34bf4c6c6c23597eb05510fa4bfbce4a9dcf1
Matias Bouin <angelmatiasbouin@gmail.com>
2020-10-13 15:16:45 -0300

    practica primer parcial c

--- estructuras de datos y algoritmos/round2/practicaPrimerParcial.c
+++ estructuras de datos y algoritmos/round2/practicaPrimerParcial.c
@@ -237,93 +237,231 @@
 //     }
 // }
 
-// >>>>>>>>>>> EJERCICIO CUATRO <<<<<<<<<<
-//Librerias
-#include<stdlib.h>
-#include<stdio.h>
+// // >>>>>>>>>>> EJERCICIO CUATRO <<<<<<<<<<
+// //Librerias
+// #include<stdlib.h>
+// #include<stdio.h>
 
-//Declaro mi struct
-typedef struct lista{
-    int num;
-    struct lista *sig;
-}nodo;
+// //Declaro mi struct
+// typedef struct lista{
+//     int num;
+//     struct lista *sig;
+// }nodo;
 
-//Prototipos de funciones
-void crear(nodo *registro);
-void mostrar(nodo *registro);
-void eliminar2(nodo* p);
+// //Prototipos de funciones
+// void crear(nodo *registro);
+// void mostrar(nodo *registro);
+// void eliminar2(nodo* p);
 
-//main
-int main() {
+// //main
+// int main() {
     
-    nodo *nodoCabeza;
+//     nodo *nodoCabeza;
 
-    nodoCabeza = (nodo *)malloc(sizeof(nodo));
+//     nodoCabeza = (nodo *)malloc(sizeof(nodo));
 
-    printf("Inserte los valores de la lista:\n");
-    crear(nodoCabeza);
+//     printf("Inserte los valores de la lista:\n");
+//     crear(nodoCabeza);
+//     printf("\n");
+
+//     //mostrar(nodo);
+//     mostrar(nodoCabeza);
+//     printf("\n");
+
+//     eliminar2(nodoCabeza);
+//     mostrar(nodoCabeza);
+
+//     return 0;
+// }
+
+// void crear(nodo *registro)
+// {
+//     int n, CORTE = 1000; //Ver declaracion de CORTE
+//     scanf("%d", &n);
+//     registro->num = n;
+
+//     if (n == CORTE) //Puedo hardcodear la condicion de corte => n == 0
+//     {
+//         registro->sig = NULL;
+//     }
+//     else
+//     {
+//         registro->sig = (nodo *)malloc(sizeof(nodo));
+//         crear(registro->sig);
+//     }
+    
+//     return;
+// }
+
+// //Mostrar con recursividad
+// void mostrar(nodo *registro)
+// {
+//     if (registro->sig != NULL)
+//     {
+//         printf("%d\n", registro->num);
+//         mostrar(registro->sig);
+//     }
+//     return;
+// }
+
+// nodo* eliminar1(nodo* p)
+// {   
+//     nodo *aux;
+//     while(p->num == 0)//condicion de eliminacion
+//     {
+//         aux=p->sig;
+//         free(p);
+//         p=aux;
+
+//     }
+//     return p;
+// }
+
+// void eliminar2(nodo* p)
+// {
+//     nodo *aux;
+//     int contador = 1;
+//     while(p->sig!=NULL && p->sig->sig!=NULL)
+//     {
+//         if(contador == 4)//condicion de eliminacion (Borrar el 5to elemento)
+//         {
+//             aux=p->sig->sig;
+//             free(p->sig);
+//             p->sig=aux;
+//         }
+//         else
+//         {
+//             p=p->sig;
+//         }
+//         contador++;
+//     }
+// }
+
+// >>>>>>>>>>> MODELO PARCIAL EN CLASE <<<<<<<<<<
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct lista{
+	int num;
+	struct lista *sig;
+ }nodo;
+
+
+void crear (nodo *pt);
+void mostrar(nodo *pt);
+nodo* eliminar1(nodo* , int);
+void insertar2 (nodo*, int);
+void eliminar2(nodo* , int, int);
+
+
+int main()
+{
+    
+    nodo *prin=NULL;
+    prin=(nodo*)malloc(sizeof(nodo));
+
+    crear(prin);
+    mostrar (prin);
+    printf("\n");
+    
+    int a, b, c;
+
+    printf("Ingrese dos numeros enteros: \n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+
     printf("\n");
 
-    //mostrar(nodo);
-    mostrar(nodoCabeza);
-    printf("\n");
+    if (a == 1)
+    {
+        prin = eliminar1(prin, b);
+    } else if (a > 1)
+    {
+        eliminar2(prin, a, b);
+    } else
+    {
+        printf("No se puede realizar la operacion\n");
+    }
+    
+    mostrar(prin);
 
-    eliminar2(nodoCabeza);
-    mostrar(nodoCabeza);
+
+    printf("Ingrese otro numero entero: \n");
+    scanf("%d", &c);
+
+    insertar2(prin, c);
+    mostrar(prin);
 
     return 0;
 }
 
-void crear(nodo *registro)
+void crear (nodo *registro)
 {
-    int n, CORTE = 1000; //Ver declaracion de CORTE
-    scanf("%d", &n);
-    registro->num = n;
 
-    if (n == CORTE) //Puedo hardcodear la condicion de corte => n == 0
-    {
-        registro->sig = NULL;
-    }
+    scanf("%d",&registro->num);
+
+    if (registro->num==1000)
+        registro->sig=NULL;
     else
-    {
-        registro->sig = (nodo *)malloc(sizeof(nodo));
-        crear(registro->sig);
-    }
-    
-    return;
+	{
+        registro->sig=(nodo*)malloc(sizeof(nodo));
+        crear (registro->sig);
+
+	}
+	return;
 }
 
-//Mostrar con recursividad
-void mostrar(nodo *registro)
+void mostrar (nodo *registro)
 {
-    if (registro->sig != NULL)
-    {
-        printf("%d\n", registro->num);
-        mostrar(registro->sig);
-    }
-    return;
+    if (registro->sig !=NULL)
+	{
+        printf ("%d\n",registro->num);
+        mostrar (registro->sig);
+	}
+	return;
 }
 
-nodo* eliminar1(nodo* p)
-{   
+void insertar2 (nodo *p, int c)
+{
     nodo *aux;
-    while(p->num == 0)//condicion de eliminacion
+    int contador = 1, valor = c;
+
+    while(p->sig!=NULL)
+    {
+        if(contador >= c && valor >= 0) //condicion de insercion
+        {
+            aux=(nodo *)malloc(sizeof(nodo));
+            aux->num=valor;//valor a insertar
+            aux->sig=p->sig;
+            p->sig=aux;
+            valor--;
+        }
+        p=p->sig;
+        contador++;
+    }
+}
+
+nodo* eliminar1(nodo* p, int b)
+{
+    nodo *aux;
+    int contador=1;
+    while(contador <= b)//condicion de eliminacion
     {
         aux=p->sig;
         free(p);
         p=aux;
-
+        contador++;
     }
     return p;
 }
-
-void eliminar2(nodo* p)
-{
+void eliminar2(nodo* p, int a, int b)
+{   
     nodo *aux;
-    int contador = 1;
-    while(p->sig!=NULL && p->sig->sig!=NULL)
+    int contador=1;
+    while(p->sig!=NULL&& p->sig->sig!=NULL)
     {
-        if(contador == 4)//condicion de eliminacion (Borrar el 5to elemento)
+        if(contador >= a && contador <= b)//condicion de eliminacion
         {
             aux=p->sig->sig;
             free(p->sig);
@@ -336,3 +474,5 @@ void eliminar2(nodo* p)
         contador++;
     }
 }
+
+// >>>>>>>>>>> MODELO PARCIAL EN CLASE <<<<<<<<<<
